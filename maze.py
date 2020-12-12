1c128e8dd6e5f4c096ba41508ce4daa3c30798ff
redr0b0t <jatinyadav5511@gmail.com>
2020-12-12 16:30:36 +0530

    feat:main menu

--- maze.py
+++ maze.py
@@ -25,7 +25,7 @@ COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
 COLOR_RED = (255, 0, 0)
 COLOR_GREEN = (0, 255, 0)
-COLOR_BLUE = (255,160,122)
+COLOR_BLUE = (255, 160, 122)
 COLOR_CYAN = (0, 255, 255)
 
 FONT_SIZE = 16
@@ -35,6 +35,8 @@ BUTTONS = []
 
 SOLVE_THREAD = None
 
+r1 = r2 = 0
+
 
 def draw_rect(x, y, len, color):
     pygame.draw.rect(SCREEN, color, [x, y, len, len], 0)
@@ -42,6 +44,13 @@ def draw_rect(x, y, len, color):
 
 def draw_button(x, y, len, height, text):
     pygame.draw.rect(SCREEN, COLOR_BLACK, [x, y, len, height], 1)
+    text_surface = FONT.render(text, True, COLOR_BLACK)
+    text_len = text.__len__() * FONT_SIZE
+    SCREEN.blit(text_surface, (x + (len - text_len) / 2, y + 2))
+
+
+def draw_text(x, y, len, text):
+    # pygame.draw.rect(SCREEN, COLOR_BLACK, [x, y, len, height], 1)
     text_surface = FONT.render(text, True, COLOR_BLACK)
     text_len = text.__len__() * FONT_SIZE
     SCREEN.blit(text_surface, (x + (len - text_len) / 2, y + 2))
@@ -58,17 +67,80 @@ def refresh():
     SOLVE_THREAD.start()
 
 
+def draw_menu():
+    SCREEN.fill(COLOR_CYAN)
+    draw_text(2, 2, WIDTH - 4, 'Menu')
+    draw_button(2, 60, WIDTH - 4, HEADER - 4, 'Easy')
+    draw_button(2, 100, WIDTH - 4, HEADER - 4, 'Medium')
+    draw_button(2, 140, WIDTH - 4, HEADER - 4, 'Hard')
+
+    draw_text(200, 240, WIDTH - 4, 'Jatin Yadav')
+    draw_text(200, 280, WIDTH - 4, 'Gopi kishan')
+    draw_text(200, 320, WIDTH - 4, 'Kishore H')
+
+    # if len(BUTTONS) == 0:
+    BUTTONS.append({
+        'x': 2,
+        'y': 60,
+        'length': WIDTH - 4,
+        'height': HEADER - 4,
+        'click': easy
+    })
+
+    BUTTONS.append({
+        'x': 2,
+        'y': 100,
+        'length': WIDTH - 4,
+        'height': HEADER - 4,
+        'click': medium
+    })
+
+    BUTTONS.append({
+        'x': 2,
+        'y': 140,
+        'length': WIDTH - 4,
+        'height': HEADER - 4,
+        'click': hard
+    })
+
+    pygame.display.flip()
+
+
+def easy():
+    global r1
+    global r2
+    r1 = 5
+    r2 = 10
+    refresh()
+
+
+def medium():
+    global r1
+    global r2
+    r1 = 10
+    r2 = 15
+    refresh()
+
+
+def hard():
+    global r1
+    global r2
+    r1 = 15
+    r2 = 20
+    refresh()
+
+
 def draw_maze(maze, cur_pos):
     SCREEN.fill(COLOR_WHITE)
     draw_button(2, 2, WIDTH - 4, HEADER - 4, 'Toggle Level')
-    if len(BUTTONS) == 0:
-        BUTTONS.append({
-            'x': 2,
-            'y': 2,
-            'length': WIDTH - 4,
-            'height': HEADER - 4,
-            'click': refresh
-        })
+    BUTTONS.clear()
+    BUTTONS.append({
+        'x': 2,
+        'y': 2,
+        'length': WIDTH - 4,
+        'height': HEADER - 4,
+        'click': refresh
+    })
 
     size = len(maze)
     cell_size = int(WIDTH / size)
@@ -92,15 +164,15 @@ def dispatcher_click(pos):
 
 
 def random_maze_size():
-    return random.randint(5, 20) * 2 + 1
+    return random.randint(r1, r2) * 2 + 1
+    # return random.randint(5, 20) * 2 + 1
 
 
-if __name__ == '__main__':
-    size = random_maze_size()
-    MAZE, ENTRANCE, EXIT = generate_maze(size, size)
-    SOLVE_THREAD = threading.Thread(target=solve_maze, args=(MAZE, ENTRANCE, EXIT, draw_maze))
-    SOLVE_THREAD.start()
-    while True:
+def menu():
+    global SOLVE_THREAD
+    level = 0
+    draw_menu()
+    while not level:
         CLOCK.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -111,3 +183,25 @@ if __name__ == '__main__':
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
                 dispatcher_click(mouse_pos)
+
+
+if __name__ == '__main__':
+    # menu
+    menu()
+
+    # game
+    # size = random_maze_size()
+    # MAZE, ENTRANCE, EXIT = generate_maze(size, size)
+    # SOLVE_THREAD = threading.Thread(target=solve_maze, args=(MAZE, ENTRANCE, EXIT, draw_maze))
+    # SOLVE_THREAD.start()
+    # while True:
+    #     CLOCK.tick(FPS)
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             if SOLVE_THREAD is not None and SOLVE_THREAD.is_alive():
+    #                 stop_thread(SOLVE_THREAD)
+    #                 SOLVE_THREAD = None
+    #             exit(0)
+    #         elif event.type == pygame.MOUSEBUTTONDOWN:
+    #             mouse_pos = pygame.mouse.get_pos()
+    #             dispatcher_click(mouse_pos)
